098c5f5181ad6b532ab9683bff8af1235556b0ac
mseaton <mseaton@pih.org>
2014-10-29 16:46:21 -0400

    Add borrowed code from databaseexporter project and get initial connectivity to oracle working

---
+++ common/src/main/java/org/openmrs/hivmigration/common/util/Util.java
@@ -0,0 +1,133 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.hivmigration.common.util;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+public class Util {
+
+	public static String toString(Collection<?> c) {
+		StringBuilder ret = new StringBuilder();
+		if (c != null) {
+			for (Object o : c) {
+				ret.append(ret.length() == 0 ? "" : ",").append(o);
+			}
+		}
+		return ret.toString();
+	}
+
+	public static String toString(Object[] c) {
+		if (c != null) {
+			return toString(Arrays.asList(c));
+		}
+		return "";
+	}
+
+	public static List<String> toList(String s, String separator) {
+		List<String> ret = new ArrayList<String>();
+		for (String element : s.split(separator)) {
+			ret.add(element);
+		}
+		return ret;
+	}
+
+	public static boolean isEmpty(Object o) {
+		return o == null || o.equals("");
+	}
+
+	public static boolean notEmpty(Object o) {
+		return !isEmpty(o);
+	}
+
+	public static <T> T firstNotNull(T... values) {
+		for (T val : values) {
+			if (notEmpty(val)) {
+				return val;
+			}
+		}
+		return null;
+	}
+
+	public static <T> T nvl(T o, T valueIfNull) {
+		if (isEmpty(o)) {
+			return valueIfNull;
+		}
+		return o;
+	}
+
+	public static String nvlStr(Object o, String valueIfNull) {
+		if (isEmpty(o)) {
+			return valueIfNull;
+		}
+		return o.toString();
+	}
+
+	public static boolean areEqualStr(Object o1, Object o2) {
+		if (o1 == o2) {
+			return true;
+		}
+		return nvlStr(o1, "").equals(nvlStr(o2, ""));
+	}
+
+	public static String formatDate(Date d, String format) {
+		SimpleDateFormat df = new SimpleDateFormat(format);
+		return df.format(d);
+	}
+
+	public static String loadFromFile(String path) {
+		String ret = null;
+		try {
+			ret = FileUtils.readFileToString(new File(path), "UTF-8");
+		}
+		catch (Exception e) {}
+		return ret;
+	}
+
+	public static String encodeString(String strToEncode) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			byte[] input = strToEncode.getBytes("UTF-8");
+			return hexString(md.digest(input));
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Unable to encode string " + strToEncode, e);
+		}
+	}
+
+	public static String hexString(byte[] block) {
+		StringBuffer buf = new StringBuffer();
+		char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		int len = block.length;
+		int high = 0;
+		int low = 0;
+		for (int i = 0; i < len; i++) {
+			high = ((block[i] & 0xf0) >> 4);
+			low = (block[i] & 0x0f);
+			buf.append(hexChars[high]);
+			buf.append(hexChars[low]);
+		}
+		return buf.toString();
+	}
+
+}
+
--- export/src/main/java/org/openmrs/hivmigration/export/Exporter.java
+++ export/src/main/java/org/openmrs/hivmigration/export/Exporter.java
@@ -1,5 +1,82 @@
 package org.openmrs.hivmigration.export;
 
+import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.ColumnListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Exporter {
 
+	//***** SINGLETON SUPPORT *****
+
+	private static Exporter exporter;
+	private Exporter() {}
+
+	//***** INSTANCE VARIABLES *****
+	private Configuration configuration;
+
+	//***** PUBLIC *****
+
+	public static Exporter initialize(Configuration configuration) {
+		exporter = new Exporter();
+		exporter.configuration = configuration;
+		return exporter;
+	}
+
+	public <T> T executeQuery(String sql, ResultSetHandler<T> handler, Object...params) {
+		Connection connection = null;
+		try {
+			QueryRunner runner = new QueryRunner() {
+				protected PreparedStatement prepareStatement(Connection conn, String sql) throws SQLException {
+					PreparedStatement ps = super.prepareStatement(conn, sql);
+					//ps.setFetchSize(Integer.MIN_VALUE);
+					return ps;
+				}
+			};
+			connection = openConnection();
+			T result =  runner.query(connection, sql, handler, params);
+			return result;
+		}
+		catch (Exception e) {
+			throw new RuntimeException("Unable to execute query: " + sql, e);
+		}
+		finally {
+			DbUtils.closeQuietly(connection);
+		}
+	}
+
+	public <T> T uniqueResult(String sql, Class<T> type) {
+		return executeQuery(sql, new ScalarHandler<T>());
+	}
+
+	public <T> List<T> listResult(String sql, Class<T> type) {
+		return executeQuery(sql, new ColumnListHandler<T>());
+	}
+
+	public List<String> getAllTables() {
+		String allTableQuery = "select distinct(upper(table_name)) from user_tab_columns order by upper(table_name) asc";
+		return listResult(allTableQuery, String.class);
+	}
+
+	private Connection openConnection() {
+		try {
+			DatabaseCredentials credentials = configuration.getDatabaseCredentials();
+			DbUtils.loadDriver(credentials.getDriver());
+			return DriverManager.getConnection(credentials.getUrl(), credentials.getUser(), credentials.getPassword());
+		}
+		catch (Exception e) {
+			throw new IllegalArgumentException("Error retrieving connection to the database", e);
+		}
+	}
+
+	public Configuration getConfiguration() {
+		return configuration;
+	}
 }
