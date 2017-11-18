fe1cbfc19baa14127cee30a7eb8e7f370f49e4cc
Cosmin Ioan <cioan@pih.org>
2017-11-18 10:45:14 -0500

    UHM-3105, migrate patients phone numbers

--- etl/src/main/resources/jobs/patients/revert_script.sql
+++ etl/src/main/resources/jobs/patients/revert_script.sql
@@ -4,6 +4,7 @@ delete from patient where patient_id in (select person_id from hivmigration_pati
 
 delete from name_phonetics;
 delete from obs where person_id in (select person_id from hivmigration_patients);
+delete from person_attribute where person_id in (select person_id from hivmigration_patients);
 delete from person_address where person_id in (select person_id from hivmigration_patients);
 delete from person_name where person_id in (select person_id from hivmigration_patients);
 delete from person where person_id in (select person_id from hivmigration_patients);
