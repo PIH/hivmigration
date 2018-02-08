28827afef7a1073be9d4c9cc8ccd900fb0120d2f
mgoodrich <mark.goodrich@live.com>
2018-02-08 10:24:16 -0500

    add "lab_result" to the list of imported encounters

--- etl/src/main/resources/jobs/encounters/hivmigration_encounter_type_lookup.sql
+++ etl/src/main/resources/jobs/encounters/hivmigration_encounter_type_lookup.sql
@@ -8,8 +8,9 @@ create table hivmigration_encounter_type (
 
 SET @encounter_type_intake = (select encounter_type_id from encounter_type where uuid = 'c31d306a-40c4-11e7-a919-92ebcb67fe33');
 SET @encounter_type_followup = (select encounter_type_id from encounter_type where uuid = 'c31d3312-40c4-11e7-a919-92ebcb67fe33');
+SET @encounter_type_specimen_collection = (select encounter_type_id from encounter_type where uuid = '10db3139-07c0-4766-b4e5-a41b01363145');
 
 insert into hivmigration_encounter_type(encounter_type, encounter_type_id) values("intake", @encounter_type_intake);
 insert into hivmigration_encounter_type(encounter_type, encounter_type_id) values("followup", @encounter_type_followup);
-
+insert into hivmigration_encounter_type(encounter_type, encounter_type_id) values("lab_result", @encounter_type_specimen_collection);
 
