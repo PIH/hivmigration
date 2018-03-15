7fc9690b94f4081379b588866ae84fdc3730aecd
Cosmin Ioan <cioan@pih.org>
2018-03-15 13:30:24 -0400

    UHM-3235, fix the concurency issue when inserting lab results

--- etl/src/main/resources/jobs/obs/insert_vl_results.sql
+++ etl/src/main/resources/jobs/obs/insert_vl_results.sql
@@ -11,6 +11,16 @@ BEGIN
   DECLARE test_name int;
   DECLARE exa_vir int;
 
+  DECLARE cd4_construct int;
+  DECLARE cd4_value int;
+  DECLARE hematocrit_construct int;
+  DECLARE hematocrit_value int;
+  DECLARE ppd_construct int;
+  DECLARE ppd_value int;
+  DECLARE test_rapid_construct int;
+  DECLARE positive int;
+  DECLARE negative int;
+
   -- HIV Viral Load Construct
   select concept_id INTO hiv_vl_construct from concept where uuid='11765b8c-a338-48a4-9480-df898c903723';
 
@@ -20,6 +30,43 @@ BEGIN
     from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
       join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='viral_load';
 
+  -- CD4 Count Construct
+  select concept_id INTO @cd4_construct from concept where uuid='37769FDB-5FC1-4D47-82C2-DB88960BB224';
+
+  -- add OBS_GROUP  CD4 Count Construct
+  insert into obs(obs_id, person_id, concept_id, encounter_id, obs_datetime, location_id, creator, date_created, voided, uuid)
+    select r.obs_id, p.person_id, @cd4_construct, e.encounter_id, r.obs_datetime, e.location_id, 1, now(), 0, r.uuid
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='cd4';
+
+  -- hematocrit Construct
+  select concept_id INTO @hematocrit_construct from concept where uuid='267C165C-1B8F-48FE-91AC-C1AE8C7412A0';
+
+  -- add OBS_GROUP  hematocrit Construct
+  insert into obs(obs_id, person_id, concept_id, encounter_id, obs_datetime, location_id, creator, date_created, voided, uuid)
+    select r.obs_id, p.person_id, @hematocrit_construct, e.encounter_id, r.obs_datetime, e.location_id, 1, now(), 0, r.uuid
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='hematocrit';
+
+  -- PPD Construct
+  select concept_id INTO @ppd_construct from concept where uuid='ACF90DED-B595-4356-9840-788094C60AFB';
+
+  -- add OBS_GROUP  PPD Construct
+  insert into obs(obs_id, person_id, concept_id, encounter_id, obs_datetime, location_id, creator, date_created, voided, uuid)
+    select r.obs_id, p.person_id, @ppd_construct, e.encounter_id, r.obs_datetime, e.location_id, 1, now(), 0, r.uuid
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='ppd';
+
+  -- test_rapid Construct
+  select concept_id INTO @test_rapid_construct from concept where uuid='A19F5A83-E960-413D-B93B-9270C53580A2';
+
+  -- add OBS_GROUP  test_rapid Construct
+  insert into obs(obs_id, person_id, concept_id, encounter_id, obs_datetime, location_id, creator, date_created, voided, uuid)
+    select r.obs_id, p.person_id, @test_rapid_construct, e.encounter_id, r.obs_datetime, e.location_id, 1, now(), 0, r.uuid
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='tr';
+
+  -- Finish loading viral load results
   -- Specimen Number
   select concept_id INTO specimen_number from concept where uuid='162086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 
@@ -56,5 +103,82 @@ BEGIN
     from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
       join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
     where r.vl_detectable_lower_limit is not null and r.test_type ='viral_load';
+
+  -- Specimen Number
+  select concept_id INTO @specimen_number from concept where uuid='162086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_text, creator, date_created, voided, uuid)
+    select p.person_id, @specimen_number, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.sample_id,1, now(), 0,  uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='cd4';
+
+
+  -- CD4 Count Numeric Value
+  select concept_id INTO @cd4_value from concept where uuid='3ceda710-26fe-102b-80cb-0017a47871b2';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_numeric, creator, date_created, voided, uuid)
+    select p.person_id, @cd4_value, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.value_numeric,1, now(), 0, uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
+    where r.value_numeric is not null and r.test_type ='cd4';
+
+  -- Specimen Number
+  select concept_id INTO @specimen_number from concept where uuid='162086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_text, creator, date_created, voided, uuid)
+    select p.person_id, @specimen_number, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.sample_id,1, now(), 0,  uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='hematocrit';
+
+
+  -- hematocrit Count Numeric Value
+  select concept_id INTO @hematocrit_value from concept where uuid='3cd69a98-26fe-102b-80cb-0017a47871b2';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_numeric, creator, date_created, voided, uuid)
+    select p.person_id, @hematocrit_value, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.value_numeric,1, now(), 0, uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
+    where r.value_numeric is not null and r.test_type ='hematocrit';
+
+  -- Specimen Number
+  select concept_id INTO @specimen_number from concept where uuid='162086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_text, creator, date_created, voided, uuid)
+    select p.person_id, @specimen_number, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.sample_id,1, now(), 0,  uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='ppd';
+
+
+  -- ppd Count Numeric Value
+  select concept_id INTO @ppd_value from concept where uuid='3cecf388-26fe-102b-80cb-0017a47871b2';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_numeric, creator, date_created, voided, uuid)
+    select p.person_id, @ppd_value, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.value_numeric,1, now(), 0, uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
+    where r.value_numeric is not null and r.test_type ='ppd';
+
+  -- Specimen Number
+  select concept_id INTO @specimen_number from concept where uuid='162086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_text, creator, date_created, voided, uuid)
+    select p.person_id, @specimen_number, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.sample_id,1, now(), 0,  uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='tr';
+
+  -- Test rapid coded answer
+  select concept_id INTO @test_rapid_value from concept where uuid='3cd6c946-26fe-102b-80cb-0017a47871b2';
+  select concept_id INTO @positive from concept where uuid='3cd3a7a2-26fe-102b-80cb-0017a47871b2';
+  select concept_id INTO @negative from concept where uuid='3cd28732-26fe-102b-80cb-0017a47871b2';
+
+  insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_coded, creator, date_created, voided, uuid)
+    select p.person_id, @test_rapid_value, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id,
+      case when ( r.value_boolean=1) then (@positive)
+      else @negative
+      end as value_coded
+      ,1, now(), 0, uuid()
+    from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
+      join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
+    where r.test_type ='tr';
 
 END
