9d3daec978c3e103741502bfa5ad55796b455679
Dominic Surrao <dsurrao@hotmail.com>
2018-06-15 18:36:05 -0400

    move hiv_observations to staging table

---
+++ etl/src/main/resources/jobs/observations/staging_schema.sql
@@ -0,0 +1,14 @@
+drop table if exists hivmigration_observations;
+
+create table hivmigration_observations (
+  observation_id int PRIMARY KEY AUTO_INCREMENT,
+  source_observation_id int,
+  source_encounter_id int,
+  observation varchar(100),
+  value varchar(4000),
+  entry_date date,
+  observation_uuid char(38),
+  KEY `source_observation_idx` (`source_observation_id`),
+  KEY `source_encounter_idx` (`source_encounter_id`),
+  UNIQUE KEY `uuid_idx` (`observation_uuid`)
+);
