e7dbdbd6e3c7734cd9b392000f5630cb9b2f7f57
Brandon Istenes <bistenes@gmail.com>
2020-08-26 13:34:57 -0700

    Start work on programs

---
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/ProgramMigrator.groovy
@@ -0,0 +1,108 @@
+package org.pih.hivmigration.etl.sql
+
+class ProgramMigrator extends SqlMigrator {
+
+    void migrate() {
+        executeMysql("Create staging table to receive program data", '''
+            create table hivmigration_programs_initial (
+                source_patient_id int PRIMARY KEY,
+                starting_health_center int,
+                health_center int,
+                health_center_transfer_date date,
+                treatment_status varchar(30),
+                treatment_status_date date,
+                start_date date,
+                regimen_outcome varchar(20),
+                regimen_outcome_date date,
+                min_visit_date date,
+                max_visit_date date
+            );
+        ''')
+
+        executeMysql("Create staging table for processing program data", '''
+            create table hivmigration_programs (
+                patient_program_id int PRIMARY KEY AUTO_INCREMENT,
+                source_patient_id int,
+                location_id int,
+                enrollment_date date,
+                art_start_date date,
+                outcome_date date,
+                outcome varchar(255)
+            );
+        ''')
+
+        executeMysql("Create outcome lookup table", '''
+            create table hivmigration_outcome (
+              id int PRIMARY KEY AUTO_INCREMENT,
+            	outcome varchar(255),
+            	outcome_concept_id int
+            );
+            
+            SET @outcome_died = (select concept_id from concept where uuid = '3cdd446a-26fe-102b-80cb-0017a47871b2');
+            SET @outcome_lost_to_followup = (select concept_id from concept where uuid = '3ceb0ed8-26fe-102b-80cb-0017a47871b2');
+            SET @outcome_transferred_out = (select concept_id from concept where uuid = '3cdd5c02-26fe-102b-80cb-0017a47871b2');
+            SET @outcome_treatment_stopped = (select concept_id from concept where uuid = '3cdc0d7a-26fe-102b-80cb-0017a47871b2');
+            
+            insert into hivmigration_outcome(outcome, outcome_concept_id) values("DIED", @outcome_died);
+            insert into hivmigration_outcome(outcome, outcome_concept_id) values("ABANDONED", @outcome_lost_to_followup);
+            insert into hivmigration_outcome(outcome, outcome_concept_id) values("TREATMENT_STOPPED", @outcome_treatment_stopped);
+            insert into hivmigration_outcome(outcome, outcome_concept_id) values("TRANSFERRED_OUT", @outcome_transferred_out);
+            insert into hivmigration_outcome(outcome, outcome_concept_id) values("TRANSFERRED_INTERNALLY", @outcome_transferred_out);
+        ''')
+
+        setAutoIncrement("patient_program", "(select ifnull(max(patient_program_id)+1, 1) from patient_program)")
+        
+        loadFromOracleToMySql('''
+            insert into hivmigration_programs_initial
+                (source_patient_id,
+                starting_health_center,
+                health_center,
+                health_center_transfer_date,
+                treatment_status,
+                treatment_status_date,
+                start_date,
+                regimen_outcome,
+                regimen_outcome_date,
+                min_visit_date,
+                max_visit_date) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', '''
+            select 	
+                d.patient_id as source_patient_id,
+                d.starting_health_center,
+                d.health_center,
+                d.health_center_transfer_date,
+                d.treatment_status,
+                d.treatment_status_date,
+                r.start_date as art_start_date,
+                o.regimen_outcome,
+                o.regimen_outcome_date,
+                e.min_visit_date,
+                e.max_visit_date
+            from
+                hiv_demographics_real d, hiv_art_regimen_start_date r, hiv_reg_outcome_helper_dates o,
+                (
+                    select 		patient_id, min(encounter_date) as min_visit_date, max(encounter_date) as max_visit_date 
+                    from 		hiv_encounters 
+                    where 		type in ('intake','followup','patient_contact','accompagnateur') 
+                    and			encounter_date is not null
+                    group by 	patient_id
+                ) e
+            where	  
+                d.TREATMENT_STATUS != 'test' 
+            and 
+                d.patient_id = e.patient_id
+            and
+                d.patient_id = r.patient_id(+)
+            and
+                d.patient_id = o.patient_id(+)
+        ''')
+
+    }
+
+    void revert() {
+        executeMysql("drop table if exists hivmigration_programs_initial;")
+        executeMysql("drop table if exists hivmigration_programs;")
+        executeMysql("drop table if exists hivmigration_outcome;")
+        clearTable("program")
+    }
+}
