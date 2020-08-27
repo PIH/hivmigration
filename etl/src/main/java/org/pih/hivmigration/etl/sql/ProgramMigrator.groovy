4451e33e60f6ed7d4618ed8b2357f4d401ba18cd
Brandon Istenes <bistenes@gmail.com>
2020-08-26 18:13:34 -0700

    WIP with date logic exactly matching Pentaho script

--- etl/src/main/java/org/pih/hivmigration/etl/sql/ProgramMigrator.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/ProgramMigrator.groovy
@@ -11,9 +11,9 @@ class ProgramMigrator extends SqlMigrator {
                 health_center_transfer_date date,
                 treatment_status varchar(30),
                 treatment_status_date date,
-                start_date date,
+                art_start_date date NOT NULL,
                 regimen_outcome varchar(20),
-                regimen_outcome_date date,
+                regimen_outcome_date date NOT NULL,
                 min_visit_date date,
                 max_visit_date date
             );
@@ -60,11 +60,12 @@ class ProgramMigrator extends SqlMigrator {
                 health_center_transfer_date,
                 treatment_status,
                 treatment_status_date,
-                start_date,
+                art_start_date,
                 regimen_outcome,
                 regimen_outcome_date,
                 min_visit_date,
-                max_visit_date) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                max_visit_date)
+            values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         ''', '''
             select 	
                 d.patient_id as source_patient_id,
@@ -73,7 +74,7 @@ class ProgramMigrator extends SqlMigrator {
                 d.health_center_transfer_date,
                 d.treatment_status,
                 d.treatment_status_date,
-                r.start_date as art_start_date,
+                r.start_date,
                 o.regimen_outcome,
                 o.regimen_outcome_date,
                 e.min_visit_date,
@@ -97,12 +98,84 @@ class ProgramMigrator extends SqlMigrator {
                 d.patient_id = o.patient_id(+)
         ''')
 
+        executeMysql("Process hivmigration_programs_initial into hivmigration_programs", '''
+            insert into hivmigration_programs
+                (source_patient_id, location_id, enrollment_date, art_start_date, outcome, outcome_date)
+            select
+                source_patient_id, 
+                health_center, 
+                LEAST(IFNULL(min_visit_date, art_start_date), art_start_date), 
+                LEAST(art_start_date, 
+                CASE treatment_status
+                    WHEN died THEN DIED
+                    WHEN lost THEN ABANDONED
+                    WHEN abandoned THEN ABANDONED
+                    WHEN transferred_out THEN TRANSFERRED_OUT
+                    WHEN treatment_refused THEN TREATMENT_STOPPED
+                    WHEN treatment_stopped THEN TREATMENT_STOPPED
+                    WHEN treatment_stopped_side_effects THEN TREATMENT_STOPPED
+                    WHEN treatment_stopped_other THEN TREATMENT_STOPPED
+                    END,  
+                GREATEST(  # this is the logic that was in the Pentaho script...
+                    COALESCE(
+                        GREATEST(
+                            IFNULL(treatment_status_date, regimen_outcome_date),
+                            regimen_outcome_date
+                        ),
+                        DATE_ADD(max_visit_date, INTERVAL 6 MONTH)
+                    ),
+                    IFNULL(min_visit_date, 0)
+                    )
+            from hivmigration_programs_initial
+            where starting_health_center is not null
+        ''')
+        
+        executeMysql("Load to patient_program table", '''
+            insert into patient_program
+                (patient_program_id, patient_id, program_id, date_enrolled, date_completed, location_id, outcome_concept_id, creator, date_created, uuid)
+            select
+                h.patient_program_id, 
+                p.person_id,
+                2,
+                h.enrollment_date, 
+                h.outcome_date, 
+                hc.openmrs_id,
+                o.outcome_concept_id, 
+                1,
+                date_format(curdate(), '%Y-%m-%d %T'),
+                uuid()
+            from 
+                hivmigration_programs h 
+            inner join 
+                hivmigration_patients p on h.source_patient_id = p.source_patient_id 
+            left outer join
+                hivmigration_health_center hc on h.location_id = hc.hiv_emr_id
+            left outer join 
+                hivmigration_outcome o on h.outcome = o.outcome
+            ; 
+        ''')
+        
+        executeMysql("Add patient state", '''
+            insert into patient_state
+                (patient_program_id, state, start_date, end_date, creator, date_created, uuid)
+            select
+                patient_program_id,
+                if (art_start_date is null or art_start_date > enrollment_date, 2, 1),
+                if (art_start_date is null, enrollment_date, art_start_date), 
+                if (art_start_date is null, outcome_date, art_start_date),
+                1,
+                date_format(curdate(), '%Y-%m-%d %T'),
+                uuid()
+            from hivmigration_programs
+            ;
+            ''')
     }
 
     void revert() {
         executeMysql("drop table if exists hivmigration_programs_initial;")
         executeMysql("drop table if exists hivmigration_programs;")
         executeMysql("drop table if exists hivmigration_outcome;")
-        clearTable("program")
+        clearTable("patient_state")
+        clearTable("patient_program")
     }
 }
