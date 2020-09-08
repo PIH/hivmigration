129cf1f794eaa7fcd5dc5fdd0de14c48ddcbf7d5
Brandon Istenes <bistenes@gmail.com>
2020-09-08 09:42:17 -0700

    Some scrap changes to the Pentaho code

--- etl/src/main/resources/jobs/lab_results/insert_vl_results.sql
+++ etl/src/main/resources/jobs/lab_results/insert_vl_results.sql
@@ -2,31 +2,12 @@ CREATE PROCEDURE insert_vl_results ()
 
 BEGIN
 
-  DECLARE hiv_vl_construct int;
-  DECLARE specimen_number int;
-  DECLARE hvl_value int;
-  DECLARE hvl_qualitative int;
-  DECLARE not_detected int;
-  DECLARE detectable_lower_limit int;
-  DECLARE test_name int;
-  DECLARE exa_vir int;
-
-  DECLARE cd4_construct int;
-  DECLARE cd4_value int;
-  DECLARE hematocrit_construct int;
-  DECLARE hematocrit_value int;
-  DECLARE ppd_construct int;
-  DECLARE ppd_value int;
-  DECLARE test_rapid_construct int;
-  DECLARE positive int;
-  DECLARE negative int;
-
   -- HIV Viral Load Construct
-  select concept_id INTO hiv_vl_construct from concept where uuid='11765b8c-a338-48a4-9480-df898c903723';
+  select concept_id INTO @hiv_vl_construct from concept where uuid='11765b8c-a338-48a4-9480-df898c903723';
 
   -- add OBS_GROUP HIV Viral Load Construct
   insert into obs(obs_id, person_id, concept_id, encounter_id, obs_datetime, location_id, creator, date_created, voided, uuid)
-    select r.obs_id, p.person_id, hiv_vl_construct, e.encounter_id, r.obs_datetime, e.location_id, 1, now(), 0, r.uuid
+    select r.obs_id, p.person_id, @hiv_vl_construct, e.encounter_id, r.obs_datetime, e.location_id, 1, now(), 0, r.uuid
     from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
       join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='viral_load';
 
@@ -68,38 +49,38 @@ BEGIN
 
   -- Finish loading viral load results
   -- Specimen Number
-  select concept_id INTO specimen_number from concept where uuid='162086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+  select concept_id INTO @specimen_number from concept where uuid='162086AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 
   insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_text, creator, date_created, voided, uuid)
-    select p.person_id, specimen_number, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.sample_id,1, now(), 0, uuid()
+    select p.person_id, @specimen_number, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.sample_id,1, now(), 0, uuid()
     from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
       join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id where r.test_type ='viral_load';
 
 
   -- HIV Viral Load Numeric Value
-  select concept_id INTO hvl_value from concept where uuid='3cd4a882-26fe-102b-80cb-0017a47871b2';
+  select concept_id INTO @hvl_value from concept where uuid='3cd4a882-26fe-102b-80cb-0017a47871b2';
 
   insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_numeric, creator, date_created, voided, uuid)
-    select p.person_id, hvl_value, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.value_numeric,1, now(), 0, uuid()
+    select p.person_id, @hvl_value, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.value_numeric,1, now(), 0, uuid()
     from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
       join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
     where r.value_numeric is not null and r.test_type ='viral_load';
 
   -- HIV Viral Load Qualitative
-  select concept_id INTO hvl_qualitative from concept where uuid='1305AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
-  select concept_id INTO not_detected from concept where uuid='1302AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+  select concept_id INTO @hvl_qualitative from concept where uuid='1305AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+  select concept_id INTO @not_detected from concept where uuid='1302AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
 
   insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_coded, creator, date_created, voided, uuid)
-    select p.person_id, hvl_qualitative, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, not_detected,1, now(), 0, uuid()
+    select p.person_id, @hvl_qualitative, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, @not_detected,1, now(), 0, uuid()
     from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
       join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
     where r.vl_beyond_detectable_limit = 1 and r.test_type ='viral_load';
 
   -- Detectable Lower Limit
-  select concept_id INTO detectable_lower_limit from concept where uuid='53cb83ed-5d55-4b63-922f-d6b8fc67a5f8';
+  select concept_id INTO @detectable_lower_limit from concept where uuid='53cb83ed-5d55-4b63-922f-d6b8fc67a5f8';
 
   insert into obs(person_id, concept_id, encounter_id, obs_datetime, location_id, obs_group_id, value_numeric, creator, date_created, voided, uuid)
-    select p.person_id, detectable_lower_limit, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.vl_detectable_lower_limit,1, now(), 0, uuid()
+    select p.person_id, @detectable_lower_limit, e.encounter_id, r.obs_datetime, e.location_id, r.obs_id, r.vl_detectable_lower_limit,1, now(), 0, uuid()
     from hivmigration_lab_results r join hivmigration_patients p on r.source_patient_id = p.source_patient_id
       join hivmigration_encounters e on r.source_encounter_id = e.source_encounter_id
     where r.vl_detectable_lower_limit is not null and r.test_type ='viral_load';
