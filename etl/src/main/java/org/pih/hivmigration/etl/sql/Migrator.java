9d9d191570ece752fa979851bcdd98a3552b198b
Cosmin Ioan <cioan@pih.org>
2020-09-17 10:28:36 -0400

    UHM-4801, migrate next exam date

--- etl/src/main/java/org/pih/hivmigration/etl/sql/Migrator.java
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/Migrator.java
@@ -87,6 +87,7 @@ public class Migrator {
             if (shouldRevert || shouldRevertOnly) {
                 revert(new MedpickupsMigrator());
                 revert(new VitalsMigrator());
+                revert(new ReturnVisitDateMigrator());
                 revert(new LabResultMigrator());
                 revert(new VisitMigrator());
                 revert(new EncounterMigrator());
@@ -110,6 +111,7 @@ public class Migrator {
                 migrate(new LabResultMigrator(), limit);
                 migrate(new VitalsMigrator(), limit);
                 migrate(new MedpickupsMigrator(), limit);
+                migrate(new ReturnVisitDateMigrator(), limit);
                 if (deIdentify) {
                     migrate(new DeIdentifyMigrator(), -1);
                 }
