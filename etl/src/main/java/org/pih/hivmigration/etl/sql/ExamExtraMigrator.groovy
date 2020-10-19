cec26ef64e149d1d75ee762f22bd3c88939cbc02
Cosmin <cioan@pih.org>
2020-10-19 19:27:34 -0400

    UHM-4907, migrate transferred_out locations (#58)
    * UHM-4907, migrate transferred_out locations and transfer_p obs

--- etl/src/main/java/org/pih/hivmigration/etl/sql/ExamExtraMigrator.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/ExamExtraMigrator.groovy
@@ -59,6 +59,109 @@ class ExamExtraMigrator extends ObsMigrator {
         ''')
 
         migrate_tmp_obs()
+
+        executeMysql("Create staging table for migrating transfer_p obs", '''
+            create table hivmigration_transfer_p (                            
+              source_encounter_id int PRIMARY KEY,
+              source_patient_id int,                            
+              transfer_p BOOLEAN,               
+              obs_date date             
+            );
+        ''')
+
+        executeMysql("Create staging table for migrating transfer_out_to obs", '''
+            create table hivmigration_transfer_out_to (                            
+              source_encounter_id int PRIMARY KEY,
+              source_patient_id int,                            
+              transfer_out_to VARCHAR(48) ,               
+              obs_date date             
+            );
+        ''')
+
+        loadFromOracleToMySql('''
+            insert into hivmigration_transfer_p
+                (source_encounter_id,
+                 source_patient_id,
+                 transfer_p,
+                 obs_date)
+            values (?, ?, ?, ?)
+            ''', '''
+            select o.encounter_id as source_encounter_id,
+                    e.patient_id as source_patient_id,         
+                    case o.value 
+                        when 't' then 1 
+                        when 'f' then 0 
+                        else null
+                    end transfer_p,
+                    to_char(o.entry_date, 'yyyy-mm-dd') as obs_date 
+             from hiv_observations o, hiv_encounters e 
+             where o.ENCOUNTER_ID = e.ENCOUNTER_ID and o.observation='transfer_p' and o.VALUE is not null 
+             order by o.ENCOUNTER_ID 
+        ''')
+
+        loadFromOracleToMySql('''
+            insert into hivmigration_transfer_out_to
+                (source_encounter_id,
+                 source_patient_id,
+                 transfer_out_to,
+                 obs_date)
+            values (?, ?, ?, ?)
+            ''', '''
+            select o.encounter_id as source_encounter_id,
+                    e.patient_id as source_patient_id,         
+                    i.name as transfer_out_to,
+                    to_char(o.entry_date, 'yyyy-mm-dd') as obs_date 
+            from hiv_observations o, hiv_encounters e, hiv_institutions i  
+            where o.ENCOUNTER_ID = e.ENCOUNTER_ID and o.VALUE = i.INSTITUTION_ID 
+                    and observation='transfer_out_to' and o.value is not null 
+            order by o.ENCOUNTER_ID 
+        ''')
+
+        create_tmp_obs_table()
+
+        executeMysql("Load transferred_out observations", ''' 
+                      
+            SET @disposition_category = 'c8b22b09-e2f2-4606-af7d-e52579996de3'; 
+            SET @transferred_out = '3cdd5c02-26fe-102b-80cb-0017a47871b2';           
+            
+            INSERT INTO tmp_obs (value_coded_uuid, source_patient_id, source_encounter_id, concept_uuid)
+            SELECT @transferred_out, source_patient_id, source_encounter_id, @disposition_category
+            FROM hivmigration_transfer_p
+            WHERE transfer_p = 1;  
+                        
+            INSERT INTO tmp_obs (value_coded_uuid, source_patient_id, source_encounter_id, concept_uuid)
+            SELECT @transferred_out, source_patient_id, source_encounter_id, @disposition_category
+            FROM hivmigration_transfer_out_to
+            WHERE transfer_out_to is not null;     
+            
+            -- External transfers
+            SET @transferred_out_location = '113a5ce0-6487-4f45-964d-2dcbd7d23b67'; 
+            SET @external_location = '5b1f137c-b757-46c3-9735-c2fcb6ba221d';
+            
+            INSERT INTO tmp_obs (value_coded_uuid, source_patient_id, source_encounter_id, concept_uuid)
+            SELECT @external_location, source_patient_id, source_encounter_id, @transferred_out_location
+            FROM hivmigration_transfer_out_to
+            WHERE transfer_out_to = 'Autre (non-ZL)';
+            
+            -- Internal transfers
+            SET @internal_location = '83192b97-480c-4533-8ede-aeac15a3a736';
+            
+            INSERT INTO tmp_obs (value_coded_uuid, source_patient_id, source_encounter_id, concept_uuid)
+            SELECT @internal_location, source_patient_id, source_encounter_id, @transferred_out_location
+            FROM hivmigration_transfer_out_to
+            WHERE transfer_out_to != 'Autre (non-ZL)';
+            
+            -- ZL transfered location name
+            SET @zl_transfer_site = 'a96352e3-3afc-418b-b79f-3290fc26a3b3';
+            
+            INSERT INTO tmp_obs (value_text, source_patient_id, source_encounter_id, concept_uuid)
+            SELECT transfer_out_to, source_patient_id, source_encounter_id, @zl_transfer_site
+            FROM hivmigration_transfer_out_to
+            WHERE transfer_out_to != 'Autre (non-ZL)';
+                   
+        ''')
+
+        migrate_tmp_obs()
     }
 
     @Override
@@ -73,5 +176,7 @@ class ExamExtraMigrator extends ObsMigrator {
                     );          
         ''')
         executeMysql("drop table if exists hivmigration_exam_extra")
+        executeMysql("drop table if exists hivmigration_transfer_p")
+        executeMysql("drop table if exists hivmigration_transfer_out_to")
     }
 }
