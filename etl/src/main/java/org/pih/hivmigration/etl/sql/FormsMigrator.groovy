2537e877fe7563705937df96c0ce153bc108f415
Brandon Istenes <bistenes@gmail.com>
2020-11-20 12:13:43 -0800

    UHM-5035 Map Progress Section on HIV EMR Followup Form to Progress Section on PIH EMR Followup Form
    https://pihemr.atlassian.net/browse/UHM-5035

--- etl/src/main/java/org/pih/hivmigration/etl/sql/FormsMigrator.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/FormsMigrator.groovy
@@ -15,11 +15,27 @@ class FormsMigrator extends ObsMigrator {
             FROM hivmigration_intake_forms WHERE recommendation IS NOT NULL;
         ''')
 
+        executeMysql("Migrate progress", '''
+            INSERT INTO tmp_obs (value_coded_uuid, source_encounter_id, concept_uuid)
+            SELECT CASE progress
+                        WHEN 'acceptable' THEN concept_uuid_from_mapping('PIH', 'SATISFACTORY')
+                        WHEN 'deterioration' THEN concept_uuid_from_mapping('PIH', 'DETERIORATION')
+                        WHEN 'excellent' THEN concept_uuid_from_mapping('PIH', 'GOOD')
+                        WHEN 'good' THEN concept_uuid_from_mapping('PIH', 'GOOD')
+                        WHEN 'no_progress' THEN concept_uuid_from_mapping('CIEL', '162679')
+                        WHEN 'satisfactory' THEN concept_uuid_from_mapping('PIH', 'SATISFACTORY')
+                        END,
+                   encounter_id,
+                   concept_uuid_from_mapping('CIEL', '160116')
+            FROM hivmigration_followup_forms WHERE progress IS NOT NULL;
+        ''')
+
         migrate_tmp_obs()
     }
 
     @Override
     def void revert() {
         executeMysql("DELETE FROM obs WHERE concept_id = concept_from_mapping('CIEL', '162749')")
+        executeMysql("DELETE FROM obs WHERE concept_id = concept_from_mapping('CIEL', '160116')")
     }
 }
