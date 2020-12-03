02a31378876a6b80232028ee60c2ce408337f010
Cosmin Ioan <cioan@pih.org>
2020-12-03 17:10:17 -0500

    UHM-5055, migrate HIV_INTAKE_EXTRA table

--- etl/src/main/java/org/pih/hivmigration/etl/sql/StagingTablesMigrator.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/StagingTablesMigrator.groovy
@@ -7,10 +7,12 @@ class StagingTablesMigrator extends SqlMigrator {
         (new TableStager("HIV_FOLLOWUP_FORMS")).migrate()
         (new TableStager("HIV_DIAGNOSES")).migrate()
         (new TableStager("HIV_PATIENT_DIAGNOSES")).migrate()
+        (new TableStager("HIV_INTAKE_EXTRA")).migrate()
     }
 
     @Override
     def void revert() {
+        (new TableStager("HIV_INTAKE_EXTRA")).revert()
         (new TableStager("HIV_PATIENT_DIAGNOSES")).revert()
         (new TableStager("HIV_DIAGNOSES")).revert()
         (new TableStager("HIV_FOLLOWUP_FORMS")).revert()
