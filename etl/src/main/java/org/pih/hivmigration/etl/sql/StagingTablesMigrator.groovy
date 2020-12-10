dc815d8cb309236cc7e58b7b64c1af4697b84a21
Cosmin Ioan <cioan@pih.org>
2020-12-10 17:12:58 -0500

    UHM-5073, migrate audit tables

--- etl/src/main/java/org/pih/hivmigration/etl/sql/StagingTablesMigrator.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/StagingTablesMigrator.groovy
@@ -9,10 +9,20 @@ class StagingTablesMigrator extends SqlMigrator {
         (new TableStager("HIV_PATIENT_DIAGNOSES")).migrate()
         (new TableStager("HIV_INTAKE_EXTRA")).migrate()
         (new TableStager("HIV_PREVIOUS_EXPOSURES")).migrate()
+        (new TableStager("HIV_DATA_AUDIT")).migrate()
+        (new TableStager("HIV_DATA_AUDIT_ENTRY")).migrate()
+        (new TableStager("HIV_DATA_AUDIT_TRACKING_FORMS")).migrate()
+        (new TableStager("HIV_PATIENT_OBS")).migrate()
+        (new TableStager("HIV_COURSE_OF_TX")).migrate()
     }
 
     @Override
     def void revert() {
+        (new TableStager("HIV_COURSE_OF_TX")).revert()
+        (new TableStager("HIV_PATIENT_OBS")).revert()
+        (new TableStager("HIV_DATA_AUDIT_TRACKING_FORMS")).revert()
+        (new TableStager("HIV_DATA_AUDIT_ENTRY")).revert()
+        (new TableStager("HIV_DATA_AUDIT")).revert()
         (new TableStager("HIV_INTAKE_EXTRA")).revert()
         (new TableStager("HIV_PREVIOUS_EXPOSURES")).revert()
         (new TableStager("HIV_PATIENT_DIAGNOSES")).revert()
