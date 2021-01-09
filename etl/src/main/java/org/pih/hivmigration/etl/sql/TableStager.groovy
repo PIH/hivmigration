28566dc3a31c94dd502eb43abec123050eeb1818
Brandon Istenes <bistenes@gmail.com>
2021-01-08 18:09:06 -0800

    Improvements to TableStager; try to fix SocioEconomicsMigrator

--- etl/src/main/java/org/pih/hivmigration/etl/sql/TableStager.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/TableStager.groovy
@@ -15,22 +15,26 @@ class TableStager extends SqlMigrator {
     }
     @Override
     void migrate() {
+        def columnNameMap = [PATIENT_ID: "source_patient_id", ENCOUNTER_ID: "source_encounter_id"]
         def datatypeMap = [VARCHAR2: "TEXT", NUMBER: "NUMERIC"]
 
         List<Map<String, Object>> oracleSchema = selectOracle(
                 "select COLUMN_NAME, DATA_TYPE, DATA_LENGTH from ALL_TAB_COLUMNS where TABLE_NAME='" + oracleTableName + "'",
                 new MapListHandler())
 
+        List<String> oracleColumnNames = oracleSchema.stream()
+            .map( { row -> row.get("COLUMN_NAME").toString() })
+            .collect(Collectors.toList())
 
         List<Map.Entry<String, String>> mysqlTableInfo = oracleSchema.stream()
-            .map({ row ->             
+            .map({ row ->
                 new AbstractMap.SimpleEntry<>(
-                        row.get("COLUMN_NAME").toString().toLowerCase(),
+                        columnNameMap[row.get("COLUMN_NAME").toString()] ?: row.get("COLUMN_NAME").toString().toLowerCase(),
                         datatypeMap[row.get("DATA_TYPE")] ?: ((row.get("DATA_TYPE") == "CHAR") && row.get("DATA_LENGTH") ? (row.get("DATA_TYPE") + "(" + row.get("DATA_LENGTH") + ")") : (row.get("DATA_TYPE"))))
             })
             .collect(Collectors.toList())
 
-        List<String> columnNames = mysqlTableInfo.stream()
+        List<String> mysqlColumnNames = mysqlTableInfo.stream()
                 .map(e -> e.getKey())
                 .collect(Collectors.toList())
 
@@ -42,15 +46,15 @@ class TableStager extends SqlMigrator {
                 "CREATE TABLE " + mysqlTableName + " ( " + mysqlSchema + " );")
 
         // Prepare the load statements
-        String loadInsert = ("INSERT INTO " + mysqlTableName + " ( " + columnNames.join(", ") + " ) VALUES ( "
-            + columnNames.stream().map(n -> "?").collect(Collectors.joining(", "))
+        String loadInsert = ("INSERT INTO " + mysqlTableName + " ( " + mysqlColumnNames.join(", ") + " ) VALUES ( "
+            + mysqlColumnNames.stream().map(n -> "?").collect(Collectors.joining(", "))
             + " ) ")
-        String loadSelect = "select t." + columnNames.join(", t.") + " from " + oracleTableName + " t "
+        String loadSelect = "select t." + oracleColumnNames.join(", t.") + " from " + oracleTableName + " t "
         // Filter out test data if there is a known column to join on
-        if (columnNames.contains("patient_id")) {
-            loadSelect += " , HIV_DEMOGRAPHICS_REAL d where d.patient_id = t.patient_id"
-        } else if (columnNames.contains("encounter_id")) {
-            loadSelect += " , HIV_ENCOUNTERS e, HIV_DEMOGRAPHICS_REAL d where t.encounter_id = e.encounter_id and e.patient_id = d.patient_id"
+        if (oracleColumnNames.contains("PATIENT_ID")) {
+            loadSelect += " , HIV_DEMOGRAPHICS_REAL d where d.PATIENT_ID = t.PATIENT_ID"
+        } else if (oracleColumnNames.contains("ENCOUNTER_ID")) {
+            loadSelect += " , HIV_ENCOUNTERS e, HIV_DEMOGRAPHICS_REAL d where t.ENCOUNTER_ID = e.ENCOUNTER_ID and e.PATIENT_ID = d.PATIENT_ID"
         }
         loadFromOracleToMySql(loadInsert, loadSelect)
     }
