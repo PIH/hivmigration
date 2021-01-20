1eb19f6c716f8dfb7139ab7aff67773ebe56ec86
Brandon Istenes <bistenes@gmail.com>
2021-01-19 18:13:38 -0800

    Get deduplication working and warn about discrepancies

--- etl/src/main/java/org/pih/hivmigration/etl/sql/ContactsMigrator.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/ContactsMigrator.groovy
@@ -12,6 +12,7 @@ class ContactsMigrator extends ObsMigrator {
             CREATE TABLE hivmigration_tmp_contacts (
                 obs_group_id INT PRIMARY KEY AUTO_INCREMENT,
                 source_encounter_id INT,
+                old_contact_name VARCHAR(52),
                 first_name VARCHAR(64),
                 last_name VARCHAR(64),
                 old_relationship VARCHAR(76),
@@ -26,37 +27,43 @@ class ContactsMigrator extends ObsMigrator {
 
         executeMysql("Populate temp table of patient contacts", '''
             INSERT INTO hivmigration_tmp_contacts
-                (source_encounter_id, first_name, last_name, old_relationship, relationship_uuid, age, death_uuid, hiv_status_uuid)
+                (source_encounter_id, old_contact_name, first_name, last_name, old_relationship, relationship_uuid, age, death_uuid, hiv_status_uuid)
             SELECT he.source_encounter_id,
+                   contact_name,
                    left(trim(contact_name), char_length(trim(contact_name)) - LOCATE(' ', REVERSE(trim(contact_name)))+1) as first,
                    substring_index(trim(contact_name), ' ', -1) as last,
                    relationship,
                    CASE
-                        WHEN relationship REGEXP 'fil|enf|fiil|garcon|child\'
-                            THEN concept_uuid_from_mapping('PIH', 'GUARDIAN RELATIONSHIP TO CHILD')  -- replace with PIH:CHILD once the latter is available
-                        WHEN relationship REGEXP 'ex|part|mari|conc|femme|conjoint|epou|cop|fiance|petit|pat|husband\'
-                            THEN concept_uuid_from_mapping('PIH', 'PARTNER OR SPOUSE')
-                        WHEN relationship REGEXP 'mere|pere|mére|pére\'
-                            THEN concept_uuid_from_mapping('PIH', 'GUARDIAN')
-                        WHEN relationship REGEXP 'soeur|frere|frére\'
-                            THEN concept_uuid_from_mapping('PIH', 'SIBLING')
-                        WHEN relationship IS NULL THEN NULL
-                        ELSE concept_uuid_from_mapping('PIH', 'OTHER RELATIVE')
-                        END as relationship_uuid,
+                       WHEN relationship REGEXP 'fil|enf|fiil|garcon|child'
+                           THEN concept_uuid_from_mapping('PIH', 'GUARDIAN RELATIONSHIP TO CHILD')  -- replace with PIH:CHILD once the latter is available
+                       WHEN relationship REGEXP 'ex|part|mari|conc|femme|conjoint|epou|cop|fiance|petit|pat|husband'
+                           THEN concept_uuid_from_mapping('PIH', 'PARTNER OR SPOUSE')
+                       WHEN relationship REGEXP 'mere|pere|mére|pére'
+                           THEN concept_uuid_from_mapping('PIH', 'GUARDIAN')
+                       WHEN relationship REGEXP 'soeur|frere|frére'
+                           THEN concept_uuid_from_mapping('PIH', 'SIBLING')
+                       WHEN relationship IS NULL THEN NULL
+                       ELSE concept_uuid_from_mapping('PIH', 'OTHER RELATIVE')
+                       END as relationship_uuid,
                    TIMESTAMPDIFF(YEAR, hc.birth_date, he.encounter_date) as age,
                    IF(deceased_p = 't', concept_uuid_from_mapping('PIH', 'DEATH'), NULL) as death_uuid,
                    CASE hiv_status
-                        WHEN 'not_tested' THEN concept_uuid_from_mapping('PIH', 'NO TEST')
-                        WHEN 'unknown' THEN concept_uuid_from_mapping('PIH', 'unknown')
-                        WHEN 'positive' THEN concept_uuid_from_mapping('PIH', 'POSITIVE')
-                        WHEN 'negative' THEN concept_uuid_from_mapping('PIH', 'NEGATIVE')
-                        WHEN 'pos' THEN concept_uuid_from_mapping('PIH', 'POSITIVE')
-                        WHEN 'neg' THEN concept_uuid_from_mapping('PIH', 'NEGATIVE')
-                        END as hiv_status_uuid
-            FROM hivmigration_contacts hc
-            JOIN hivmigration_encounters he
-                ON hc.source_patient_id = he.source_patient_id
-                AND he.source_encounter_type = 'intake';
+                       WHEN 'not_tested' THEN concept_uuid_from_mapping('PIH', 'NO TEST')
+                       WHEN 'unknown' THEN concept_uuid_from_mapping('PIH', 'unknown')
+                       WHEN 'positive' THEN concept_uuid_from_mapping('PIH', 'POSITIVE')
+                       WHEN 'negative' THEN concept_uuid_from_mapping('PIH', 'NEGATIVE')
+                       WHEN 'pos' THEN concept_uuid_from_mapping('PIH', 'POSITIVE')
+                       WHEN 'neg' THEN concept_uuid_from_mapping('PIH', 'NEGATIVE')
+                       END as hiv_status_uuid
+            FROM (SELECT source_patient_id,
+                         contact_name,
+                         max(relationship) as relationship,
+                         max(birth_date) as birth_date,
+                         max(deceased_p) as 'deceased_p',
+                         max(hiv_status) as hiv_status
+                  FROM hivmigration_contacts GROUP BY source_patient_id, contact_name) hc  -- subquery to deduplicate contacts
+            JOIN (SELECT * FROM hivmigration_encounters WHERE source_encounter_type = 'intake' GROUP BY source_patient_id) he
+                ON hc.source_patient_id = he.source_patient_id;
         ''')
 
         create_tmp_obs_table()
@@ -81,7 +88,8 @@ class ContactsMigrator extends ObsMigrator {
                    source_encounter_id,
                    concept_uuid_from_mapping('PIH', 'LAST NAME'),
                    last_name
-            FROM hivmigration_tmp_contacts;
+            FROM hivmigration_tmp_contacts
+            WHERE last_name IS NOT NULL;
             
             INSERT INTO tmp_obs
                 (obs_group_id, source_encounter_id, concept_uuid, value_numeric)
@@ -89,15 +97,17 @@ class ContactsMigrator extends ObsMigrator {
                    source_encounter_id,
                    concept_uuid_from_mapping('PIH', 'AGE'),
                    age
-            FROM hivmigration_tmp_contacts;
+            FROM hivmigration_tmp_contacts
+            WHERE age IS NOT NULL;
             
             INSERT INTO tmp_obs
-            (obs_group_id, source_encounter_id, concept_uuid, value_coded_uuid)
+                (obs_group_id, source_encounter_id, concept_uuid, value_coded_uuid)
             SELECT obs_group_id,
                    source_encounter_id,
                    concept_uuid_from_mapping('CIEL', '164352'),  -- Relationship to patient
                    relationship_uuid
-            FROM hivmigration_tmp_contacts;
+            FROM hivmigration_tmp_contacts
+            WHERE relationship_uuid IS NOT NULL;
             
             INSERT INTO tmp_obs
                 (obs_group_id, source_encounter_id, concept_uuid, value_coded_uuid)
@@ -105,7 +115,8 @@ class ContactsMigrator extends ObsMigrator {
                    source_encounter_id,
                    concept_uuid_from_mapping('CIEL', '163533'),  -- Health status of contact
                    death_uuid
-            FROM hivmigration_tmp_contacts;
+            FROM hivmigration_tmp_contacts
+            WHERE death_uuid IS NOT NULL;
             
             INSERT INTO tmp_obs
                 (obs_group_id, source_encounter_id, concept_uuid, value_coded_uuid)
@@ -113,10 +124,34 @@ class ContactsMigrator extends ObsMigrator {
                    source_encounter_id,
                    concept_uuid_from_mapping('PIH', 'RESULT OF HIV TEST'),
                    hiv_status_uuid
-            FROM hivmigration_tmp_contacts;
+            FROM hivmigration_tmp_contacts
+            WHERE hiv_status_uuid IS NOT NULL;
         ''')
 
         migrate_tmp_obs()
+
+        logWarning("relationship", "concept_name_from_uuid(tmp.relationship_uuid)")
+        logWarning("birth_date", "tmp.age")
+        logWarning("deceased_p", "concept_name_from_uuid(tmp.death_uuid)")
+        logWarning("hiv_status", "concept_name_from_uuid(tmp.hiv_status_uuid)")
+    }
+
+    def void logWarning(String oracleField, String tmpSelector) {
+        executeMysql("Log discrepancies in deduplicated data for " + oracleField, '''
+            INSERT INTO hivmigration_data_warnings
+                (openmrs_patient_id, field_name, field_value, warning_type, warning_details)
+            SELECT hp.person_id,
+               \'''' + oracleField + '''\',
+               hc.agg,
+               'Duplicate contact information with discrepancies',
+               CONCAT('For contact with name ', IFNULL(hc.contact_name, '\\'\\''), '. Migrated as ', ''' + tmpSelector + ''')
+            FROM (SELECT GROUP_CONCAT(''' + oracleField + ''' SEPARATOR ', ') as agg, _hc.*
+                FROM hivmigration_contacts _hc GROUP BY source_patient_id, contact_name HAVING count(distinct ''' + oracleField + ''') > 1
+             ) hc
+                 JOIN hivmigration_patients hp ON hc.source_patient_id = hp.source_patient_id
+                 JOIN hivmigration_encounters he ON hc.source_patient_id = he.source_patient_id AND he.source_encounter_type = 'intake'
+                 JOIN hivmigration_tmp_contacts tmp ON tmp.source_encounter_id = he.source_encounter_id AND tmp.old_contact_name = hc.contact_name;
+        ''')
     }
 
     @Override
