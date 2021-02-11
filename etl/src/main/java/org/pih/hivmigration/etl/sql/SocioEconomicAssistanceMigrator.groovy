fc5dc418363a47e89c6390814803150e4b5f3188
Brandon Istenes <bistenes@gmail.com>
2021-02-11 00:12:19 -0800

    UHM-5108 pt 4: No repitition of 'other' assistance types; translations

--- etl/src/main/java/org/pih/hivmigration/etl/sql/SocioEconomicAssistanceMigrator.groovy
+++ etl/src/main/java/org/pih/hivmigration/etl/sql/SocioEconomicAssistanceMigrator.groovy
@@ -27,31 +27,31 @@ class SocioEconomicAssistanceMigrator extends ObsMigrator {
             WHERE ordered = 'tranportation_aid' AND comments IS NOT NULL AND comments != 'no';
             
             INSERT INTO tmp_obs
-            (source_encounter_id, concept_uuid, value_coded_uuid)
-            SELECT source_encounter_id,
-                   CASE comments
-                       WHEN 'recommended' THEN concept_uuid_from_mapping('PIH', 'SOCIO-ECONOMIC ASSISTANCE RECOMMENDED')
-                       ELSE concept_uuid_from_mapping('PIH', 'SOCIO-ECONOMIC ASSISTANCE ALREADY RECEIVED')
-                       END,
-                   concept_uuid_from_mapping('PIH', 'NUTRITIONAL AID')
-            FROM hivmigration_ordered_other
-            WHERE ordered = 'nutritional_aid' AND comments IS NOT NULL AND comments != 'non';
-           
-            INSERT INTO tmp_obs
             (source_encounter_id, concept_uuid, value_text)
             SELECT source_encounter_id,
                    concept_uuid_from_mapping('PIH', 'SOCIO-ECONOMIC ASSISTANCE NON-CODED'),
                    GROUP_CONCAT(value_text SEPARATOR ', ')
             FROM (
                      SELECT source_encounter_id,
-                            CONCAT(CASE ordered
-                                WHEN 'financial_aid' THEN 'Aide financière\'
-                                WHEN 'funeral_aid' THEN 'Aide pour funérailles\'
-                                WHEN 'house_assistance' THEN 'Aide au logement\'
-                                WHEN 'professional_training' THEN 'Formation professionnelle\'
-                                WHEN 'school_aid' THEN 'Aide scolaire\'
-                                WHEN 'social_assistance_other' THEN comments
-                                END, ' (', comments, ')') AS value_text
+                            CONCAT(
+                                CASE ordered
+                                   WHEN 'financial_aid' THEN 'Aide financière'
+                                   WHEN 'funeral_aid' THEN 'Aide pour funérailles'
+                                   WHEN 'house_assistance' THEN 'Aide au logement'
+                                   WHEN 'professional_training' THEN 'Formation professionnelle'
+                                   WHEN 'school_aid' THEN 'Aide scolaire'
+                                   WHEN 'social_assistance_other' THEN comments
+                                   END,
+                                IF(ordered = 'social_assistance_other', '',
+                                    CONCAT(' (',
+                                       CASE comments
+                                           WHEN 'recommended' THEN 'Recommandé'
+                                           WHEN 'already_receiving' THEN 'Reçu'
+                                           ELSE comments
+                                           END,
+                                       ')')
+                                    )
+                                ) AS value_text
                      FROM hivmigration_ordered_other
                      WHERE ordered IN ('financial_aid', 'funeral_aid', 'house_assistance', 'professional_training', 'school_aid', 'social_assistance_other')
                        AND comments IS NOT NULL AND comments NOT LIKE 'no%' AND comments NOT LIKE 'aucun') o
